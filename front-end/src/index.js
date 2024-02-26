25fdb00db55f7b9c423b402287bfb7621e4690c5
MichaelIDESSIT <michaelhenrylumanta@gmail.com>
2024-02-26 11:09:58 +0800

    Updates

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,11 @@
 /*!
 
 =========================================================
-* Argon Dashboard React - v1.1.0
+* Argon Dashboard React - v1.2.4
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
@@ -16,8 +16,8 @@
 
 */
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Switch, Redirect } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -25,16 +25,15 @@ import "assets/scss/argon-dashboard-react.scss";
 
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import AuthRoutes from "./components/PrivateRoute/AuthRoutes";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <BrowserRouter>
-    <Switch>
-      <PrivateRoute path="/admin" component={AdminLayout} />
-      <AuthRoutes path="/auth" component={AuthLayout} />
-      <Redirect from="/" to="/admin/index" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <Routes>
+      <Route path="/admin/*" element={<AdminLayout />} />
+      <Route path="/auth/*" element={<AuthLayout />} />
+      <Route path="*" element={<Navigate to="/admin/index" replace />} />
+    </Routes>
+  </BrowserRouter>
 );
--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,11 +1,11 @@
 /*!
 
 =========================================================
-* Argon Dashboard React - v1.1.0
+* Argon Dashboard React - v1.2.4
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
@@ -22,117 +22,56 @@ import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
-import ConfirmEmail from "./views/examples/ConfirmEmail";
-import EditProfile from "./views/examples/EditProfile";
-import UsersTable from "./views/examples/UsersTable";
-import ResetPassword from "./views/examples/ResetPassword";
-import ConfirmPassword from "./views/examples/ConfirmPassword";
-import ResetPasswordSuccess from "./views/examples/ResetPasswordSuccess";
 
 var routes = [
   {
     path: "/index",
     name: "Dashboard",
     icon: "ni ni-tv-2 text-primary",
-    component: Index,
+    component: <Index />,
     layout: "/admin",
-    api: false
   },
   {
     path: "/icons",
     name: "Icons",
     icon: "ni ni-planet text-blue",
-    component: Icons,
+    component: <Icons />,
     layout: "/admin",
-    api: false
   },
   {
     path: "/maps",
     name: "Maps",
     icon: "ni ni-pin-3 text-orange",
-    component: Maps,
+    component: <Maps />,
     layout: "/admin",
-    api: false
   },
   {
     path: "/user-profile",
     name: "User Profile",
     icon: "ni ni-single-02 text-yellow",
-    component: Profile,
+    component: <Profile />,
     layout: "/admin",
-    api: true
   },
   {
     path: "/tables",
     name: "Tables",
     icon: "ni ni-bullet-list-67 text-red",
-    component: Tables,
+    component: <Tables />,
     layout: "/admin",
-    api: false
   },
   {
     path: "/login",
     name: "Login",
     icon: "ni ni-key-25 text-info",
-    component: Login,
+    component: <Login />,
     layout: "/auth",
-    api: true
   },
   {
     path: "/register",
     name: "Register",
     icon: "ni ni-circle-08 text-pink",
-    component: Register,
+    component: <Register />,
     layout: "/auth",
-    api: true
   },
-  {
-    path: "/confirm-email/:id",
-    name: "Confirm Email",
-    icon: "ni ni-check-bold text-green",
-    component: ConfirmEmail,
-    layout: "/auth",
-    api: true
-  },
-  {
-    path: "/edit-profile",
-    name: "Edit Profile",
-    icon: "ni ni-ruler-pencil text-info",
-    component: EditProfile,
-    layout: "/admin",
-    api: true
-  },
-  {
-    path: "/users",
-    name: "Users",
-    icon: "ni ni-folder-17 text-pink",
-    component: UsersTable,
-    layout: "/admin",
-    api: true
-  },
-  {
-    path: "/reset-password",
-    name: "Reset Password",
-    icon: "ni ni-folder-17 text-pink",
-    component: ResetPassword,
-    layout: "/auth",
-    api: true
-  },
-  {
-    path: "/confirm-password/:id",
-    name: "Confirm Password",
-    icon: "ni ni-folder-17 text-pink",
-    component: ConfirmPassword,
-    layout: "/auth",
-    api: true
-  },
-  {
-    path: "/reset-success",
-    name: "Password Reset Confirmed",
-    icon: "ni ni-folder-17 text-pink",
-    component: ResetPasswordSuccess,
-    layout: "/auth",
-    api: false
-  }
 ];
 export default routes;
